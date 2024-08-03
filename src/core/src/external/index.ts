6ed900df5b62256768bb10ebeafb62c452da1c52
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2024-08-03 17:01:05 +0800

    fix encoding error

--- src/core/src/external/index.ts
+++ src/core/src/external/index.ts
@@ -4,9 +4,13 @@ import * as pb from './protobuf';
 export class NTQQExternaiApi {
   static sendGroupPoke(groupId: string, uin: string) {
     const proto = pb.encode({
-      1: Number(uin),
-      2: Number(groupId)
+      1: 0xed3,
+      2: 1,
+      4: {
+        1: Number(uin),
+        2: Number(groupId)
+      }
     });
-    napCatCore.session.getMsgService().sendSsoCmdReqByContend('OidbSvcTrpcTcp.0xed3_1', Buffer.from(proto).toString('hex'));
+    napCatCore.session.getMsgService().sendSsoCmdReqByContend('OidbSvcTrpcTcp.0xed3_1', Buffer.from(proto).toString('utf-8'));
   }
 }
